5da9c859a342a51286d43e6a02c87baf9331885a
Fengtong He <fengtonghe@gmail.com>
2022-11-12 22:47:33 -0600

    Added search bar at customer index

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_one_attached :image
+
+  def self.search(search)
+    if search
+      keyword = "%" + search + "%"
+      @customers = Customer.where("full_name LIKE ? OR phone_number LIKE ?", keyword, keyword)
+    else
+      Customer.all
+    end
+  end
 end
