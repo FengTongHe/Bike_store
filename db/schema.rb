8e23f81144ab1de22ff3cf45d716b810623b445d
Fengtong He <fengtonghe@gmail.com>
2022-11-21 12:48:18 -0600

    Generate model category and added to active admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_020353) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_182524) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -65,6 +65,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_020353) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "full_name"
     t.string "phone_number"
@@ -83,8 +89,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_020353) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "categories"
 end
