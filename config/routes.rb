9c9bf4db01e938c44cb5a906221498a81c97fa50
Fengtong He <fengtonghe@gmail.com>
2022-11-28 23:21:34 -0600

    Change to bulma css, issue with burder id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'about_page/index'
+  get "about_page/index"
   resources :products
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
 end
