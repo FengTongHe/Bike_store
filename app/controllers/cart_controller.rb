d5975a333ca639fcf4cf0acb39e9093acb104853
Fengtong He <fengtonghe@gmail.com>
2022-12-05 14:19:21 -0600

    added flash notice when add product to the cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,8 +2,10 @@ class CartController < ApplicationController
   def create
     # logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
+    product = Product.find(id)
     session[:shopping_cart] << id
     redirect_to root_path
+    flash[:notice] = "#{product.name} added to cart."
   end
 
   def destroy; end
