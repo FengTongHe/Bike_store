c67a5065edffac1956a8a2606c427b563fb27f3c
Fengtong He <fengtonghe@gmail.com>
2022-12-13 21:57:50 -0600

    updated address controller and added province drop down list to form

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,68 @@
 class AddressesController < InheritedResources::Base
+  # GET /addresses or /addresses.json
+  def index
+    @addresses = Address.all
+  end
+
+  # GET /addresses/1 or /addresses/1.json
+  def show
+    @address = Address.find(params[:id])
+  end
+
+  # GET /addresses/new
+  def new
+    @taxes = Tax.all
+    @address = Address.new
+  end
+
+  # GET /addresses/1/edit
+  def edit; end
+
+  # POST /addresses or /addresses.json
+  def create
+    @address = Address.new(address_params)
+
+    respond_to do |format|
+      if @address.save
+        format.html do
+          redirect_to address_url(@address), notice: "Address was successfully created."
+        end
+        format.json { render :show, status: :created, location: @address }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @address.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /addresses/1 or /addresses/1.json
+  def update
+    respond_to do |format|
+      if @address.update(address_params)
+        format.html do
+          redirect_to address_url(@address), notice: "Address was successfully updated."
+        end
+        format.json { render :show, status: :ok, location: @address }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @address.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /addresses/1 or /addresses/1.json
+  def destroy
+    @address.destroy
+
+    respond_to do |format|
+      format.html { redirect_to addresses_url, notice: "Address was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 
   private
 
-    def address_params
-      params.require(:address).permit(:name, :address, :city, :province, :email)
-    end
-
+  def address_params
+    params.require(:address).permit(:name, :address, :city, :province, :email)
+  end
 end
