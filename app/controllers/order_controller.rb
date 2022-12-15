603285aff2f397c8bde0f58e894f34ca044b61a3
Fengtong He <fengtonghe@gmail.com>
2022-12-15 00:41:20 -0600

    Creating order and enter to db

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,4 +1,14 @@
 class OrderController < ApplicationController
   def create
+    address_id = params[:address_id].to_i
+    cart_id = params[:cart_id].to_i
+    subtotal = params[:subtotal].to_i
+    pst = params[:pst].to_i
+    gst = params[:gst].to_i
+
+    @order = Order.new(address_id: address_id, customer_id: 1, cart_id: cart_id, subtotal: subtotal, pst: pst,
+                       gst: gst)
+
+    redirect_to root_path, notice: "Customer was successfully created." if @order.save
   end
 end
